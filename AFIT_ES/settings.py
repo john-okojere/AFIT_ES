934fa28a127d4ecf5df8763fc8989425b23c467a
john-okojere <johnokojere08@gmail.com>
2024-09-27 02:11:58 +0100

    modified:   AFIT_ES/__pycache__/settings.cpython-39.pyc 	modified:   AFIT_ES/__pycache__/urls.cpython-39.pyc 	modified:   AFIT_ES/settings.py 	modified:   AFIT_ES/urls.py 	modified:   requirements.txt

--- AFIT_ES/settings.py
+++ AFIT_ES/settings.py
@@ -34,7 +34,6 @@ SITE_ID = 2
 
 # Application definition
 INSTALLED_APPS = [
-    'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- AFIT_ES/urls.py
+++ AFIT_ES/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('sf-admin/',  include('admin_soft.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('user.urls')),
     path('', include('event.urls')),
